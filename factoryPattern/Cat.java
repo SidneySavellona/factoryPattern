2c04d3bdefa3850b9baccdfc19719aafc527432f
Sidney Savellona <153743474+SidneySavellona@users.noreply.github.com>
2024-09-12 18:41:54 +0800

    Add files via upload

---
+++ factoryPattern/Cat.java
@@ -0,0 +1,25 @@
+package factoryPattern;
+
+public class Cat implements Pet {
+    private static Integer noOfLives;
+
+    public static Integer getNoOfLives() {
+        return noOfLives;
+    }
+
+    public void setNoOfLives(Integer noOfLives) {
+        Cat.noOfLives = noOfLives;
+    }
+
+    @Override
+    public String makeSound() {
+        return "Meow, meow!";
+    }
+
+    @Override
+    public String play() {
+        return "Catching/pursuing moving objects!";
+    }
+
+    
+}
